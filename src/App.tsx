c6c1608c5050cd38862ee3f5204a01895d453776
ruewith <ruewith@gmail.com>
2023-02-01 16:48:13 +0300

    eighth

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import "./styles/index.scss";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames("app", {}, [theme])}>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>Эбаут</Link>
             <div>
