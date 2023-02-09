227c8af99a0dbf332b4bd6c34efed6456ebef51b
ruewith <ruewith@gmail.com>
2023-02-09 13:09:03 +0300

    16-17

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,26 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env"],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ["ru", "en"],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
+            },
+        },
+    };
+
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: "ts-loader",
@@ -40,5 +60,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
-    return [typescriptLoader, fileLoader, svgLoader, cssLoader];
+    return [babelLoader, typescriptLoader, fileLoader, svgLoader, cssLoader];
 }
